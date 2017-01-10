9af2dc04b383d66bf6e4e85ffc1b5dd5dc540515
Hong Rong <brilliant.hong@gmail.com>
2017-01-10 16:40:45 +0000

    add missing file

---
+++ Library/Al.Test/S/IsTest.cs
@@ -0,0 +1,16 @@
+﻿using Algorithm.Sorting.ElementarySorts;
+using Lib.Common.UnitTest.Sorting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Al.Test.S
+{
+    [TestClass]
+    public class IsTest
+    {
+        [TestMethod]
+        public void S_Test()
+        {
+            SortingTestHelper.SortAssert(new InsertionSort().Sort);
+        }
+    }
+}
