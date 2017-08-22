9005b8206a6b81263a3b29c62731f835aaa2813e
Hong Rong <brilliant.hong@gmail.com>
2017-08-22 20:17:05 +0100

    add winform event

---
+++ Library/CSharp.WinFormMVC.Event/Program.cs
@@ -0,0 +1,29 @@
+﻿using CSharp.WinFormMVC.Event.Controller;
+using CSharp.WinFormMVC.Event.Model;
+using CSharp.WinFormMVC.Event.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSharp.WinFormMVC.Event
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            Form view = new IncreaseView();
+            IIncreaseModel model = new IncreaseModel();
+            IIncreaseController controller = new IncreaseController((IIncreaseView)view, model);
+            Application.Run(view);
+        }
+    }
+}
---
+++ Library/WinFormsTest/Tests/EventTests.cs
@@ -0,0 +1,121 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CSharp.WinForm.Tests
+{
+    [TestClass]
+    public class EventTests
+    {
+        [TestMethod]
+        public void EventWithNoDataTest()
+        {
+            EventCounterWithNoData c = new EventCounterWithNoData(new Random().Next(10));
+            c.ThresholdReached += c_ThresholdReachedWithNoData;
+            while (true)
+            {
+                Debug.WriteLine("adding one");
+                Debug.WriteLine("increase total by one");
+                c.Add(1);
+                Thread.Sleep(1000);
+            }
+        }
+
+        private void c_ThresholdReachedWithNoData(object sender, EventArgs e)
+        {
+            Debug.WriteLine("The threshold was reached.");
+            Thread.Sleep(3000);
+            Environment.Exit(0);
+        }
+
+        [TestMethod]
+        public void EventWithDataTest()
+        {
+            EventCounterWithData c = new EventCounterWithData(new Random().Next(10));
+            c.ThresholdReachedEventHandler += c_ThresholdReachedWithData;
+            while (true)
+            {
+                Debug.WriteLine("adding one");
+                Debug.WriteLine("increase total by one");
+                c.Add(1);
+                Thread.Sleep(1000);
+            }
+        }
+
+        private void c_ThresholdReachedWithData(object sender, ThresholdReachedEventArgs e)
+        {
+            Debug.WriteLine("The threshold of {0} was reached at {1}", e.Threshold, e.TimeReached);
+            Thread.Sleep(3000);
+            Environment.Exit(0);
+        }
+    }
+
+    class EventCounterWithNoData
+    {
+        private int _threshold;
+        private int _total;
+
+        public EventCounterWithNoData(int passedThreshold)
+        {
+            _threshold = passedThreshold;
+            Debug.WriteLine("threadhold: " + _threshold);
+        }
+
+        public void Add(int x)
+        {
+            _total += x;
+            Debug.WriteLine("total: " + _total);
+            if (_total >= _threshold)
+            {
+                EventHandler handler = ThresholdReached;
+                if (handler != null)
+                {
+                    handler(this, EventArgs.Empty);
+                }
+            }
+        }
+
+        public event EventHandler ThresholdReached;
+    }
+
+    class EventCounterWithData
+    {
+        private int _threshold;
+        private int _total;
+
+        public EventCounterWithData(int passedThreshold)
+        {
+            _threshold = passedThreshold;
+        }
+
+        public void Add(int x)
+        {
+            _total += x;
+            if (_total >= _threshold)
+            {
+                ThresholdReachedEventArgs args = new ThresholdReachedEventArgs();
+                args.Threshold = _threshold;
+                args.TimeReached = DateTime.Now;
+                EventHandler<ThresholdReachedEventArgs> handler = ThresholdReachedEventHandler;
+                if (handler != null)
+                {
+                    //handler(this, args);
+                    handler.Invoke(this, args);
+                }
+            }
+        }
+
+        public event EventHandler<ThresholdReachedEventArgs> ThresholdReachedEventHandler;
+    }
+
+    public class ThresholdReachedEventArgs : EventArgs
+    {
+        public int Threshold { get; set; }
+        public DateTime TimeReached { get; set; }
+    }
+}
